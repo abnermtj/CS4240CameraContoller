ae90aa89a6251c93da8e7881c1e6cc559d6afabf
abner <abnermtj@live.com.sg>
2023-01-23 23:12:00 +0800

    Add vibration

--- sketch_jan22b/sketch_jan22b.ino
+++ sketch_jan22b/sketch_jan22b.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
-#include <AsyncMqtt_Generic.h>  // Arduino library Marvin ROGER 1.8.0
+#include <WiFiClient.h>
 
 // ---------------------------- PINOUT -------------------------------
 #define BUTTON_PIN D1
@@ -9,6 +9,7 @@
 #define LED_BLUE_PIN D6
 #define ROT_ENCODER_OUTPUT_A D5
 #define ROT_ENCODER_OUTPUT_B D3
+#define VIBRATION_PIN D2
 
 // ---------------------------- ROT ENCONDER -------------------------------
 int rotationA;
@@ -18,183 +19,155 @@ int prevRotationA;
 int prevButtonState = 0;
 int curButtonState = 0;
 
-// ---------------------------- MQTT -------------------------------
-const char* MQTT_HOST = "52.221.121.128";
-const int MQTT_PORT = 1883;
+// ---------------------------- Wifi -------------------------------
+#define SEND_DELAY_MS 1
+const char* ssid = "tolentino";     //put your wifi network name here
+const char* password = "Abner123";  //put your wifi password here
 
-const char* GeneralTopic = "general";
-const char* ButtonTopic = "button";
-const char* EncoderTopic = "encoder";
+WiFiServer server(80);
+WiFiClient client;
 
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
+// Get these values from the ipconfig
+IPAddress ip(192, 168, 86, 26);
+IPAddress gateway(192, 168, 86, 1);
+IPAddress subnet(255, 255, 255, 0);
 
-////////////////////////////
-// Helper Functions
-////////////////////////////
+int prevSendTime = 0;
 
-String float_to_string(float input) {
-  char payload[10];
-  sprintf(payload, "%.2f", input);
-  return payload;
-}
+struct packet {
+  bool isButtonPressed;
+  int rotEnconderStatus;
+} sendPacket;
 
-void onMqttConnect(bool sessionPresent) {
-  Serial.print("Connected to MQTT broker: ");
-  Serial.print(MQTT_HOST);
-  Serial.print(", port: ");
-  Serial.println(MQTT_PORT);
+// --- Vibration
+#define BUTTON_DOWN_VIBRATION_TIME_MS 120
 
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
+int vibrationState;
+int vibrationStartTime;
 
-  // MqttPublish("Connecting on WeMOS", GeneralTopic);
-  Serial.println("Test Publishing at QoS 0");
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  (void)reason;
-
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
+void loopVibration() {
+  switch (vibrationState) {
+    case 0:
+      break;
+    case 1:
+      if (millis() - vibrationStartTime > BUTTON_DOWN_VIBRATION_TIME_MS) {
+        vibrationState = 0;
+        digitalWrite(VIBRATION_PIN, LOW);
+      }
+      break;
+    default:
+      break;
   }
 }
-
-void MqttPublish(char* MQTT_message, const char* MQTT_Topic) {
-  char pubTopic[50];
-  sprintf(pubTopic, "%s/%s/", GeneralTopic, MQTT_Topic);
-  mqttClient.publish(pubTopic, 0, true, MQTT_message);
-}
-
-void onMqttMessage(char* topic, char* payload, const AsyncMqttClientMessageProperties& properties,
-                   const size_t& len, const size_t& index, const size_t& total) {
-  (void)payload;
-
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-}
-
-
 void loopButton() {
   curButtonState = digitalRead(BUTTON_PIN);
+  sendPacket.isButtonPressed = curButtonState;
+  // Serial.print("Button: ");
   // Serial.println(curButtonState);
+
   if (curButtonState != prevButtonState) {
     if (curButtonState == HIGH) {
       Serial.println("Button: just released");
-      // MqttPublish("Button: just released", ButtonTopic);
       digitalWrite(LED_BLUE_PIN, LOW);
     } else {
       Serial.println("Button: just pressed");
-      // MqttPublish("Button: just pressed", ButtonTopic);
       digitalWrite(LED_BLUE_PIN, HIGH);
+      analogWrite(VIBRATION_PIN, 240);
+      vibrationStartTime = millis();
+      vibrationState = 1;
     }
   }
 
   prevButtonState = curButtonState;
-
-  char buttonData[100];
-  sprintf(buttonData, "%d", curButtonState);
-  MqttPublish(buttonData, ButtonTopic);
 }
 
 void loopRotEncoder() {
   rotationA = digitalRead(ROT_ENCODER_OUTPUT_A);
   if (rotationA != prevRotationA) {
-    //  The encoder is rotating clockwise
     if (digitalRead(ROT_ENCODER_OUTPUT_B) != rotationA) {
       Serial.println("Rotation Encoder: Clockwise Rotation");
+      sendPacket.rotEnconderStatus = 2;
     } else {
-
       Serial.println("Rotation Encoder: Anti-Clockwise Rotation");
+      sendPacket.rotEnconderStatus = 1;
+    }
+    prevRotationA = rotationA;
+  }
+}
+
+  void loop() {
+    loopButton();
+    loopRotEncoder();
+    loopVibration();
+
+    client = server.accept();
+    int curTime = millis();
+    if (client && (curTime - prevSendTime >= SEND_DELAY_MS)) {
+      prevSendTime = curTime;
+
+      // Packet structure
+      // Bit 2-3 Rot encoder status
+      // Bit 1 Button status
+      int packet = (sendPacket.rotEnconderStatus << 1) | (sendPacket.isButtonPressed);
+      client.print(packet);
+
+      sendPacket.rotEnconderStatus = 0;
     }
   }
-  prevRotationA = rotationA;
-}
 
-void loop() {
-  loopButton();
-  loopRotEncoder();
-}
-
-
-inline void initRotEncoder() {
-  pinMode(ROT_ENCODER_OUTPUT_A, INPUT);
-  pinMode(ROT_ENCODER_OUTPUT_B, INPUT);
-
-  prevRotationA = digitalRead(ROT_ENCODER_OUTPUT_A);
-}
-
-void connectToMqtt() 
-{
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
-}
-
-void initMqtt() {
-  connectToMqtt();
-
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  Serial.println("MQTT: Setup done");
-}
-
-inline void initWifi() {
-  WiFi.begin("tolentino", "Abner123");
-  Serial.print("Wifi: Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  inline void initVibration() {
+    pinMode(VIBRATION_PIN, OUTPUT);
   }
-  Serial.println();
+  inline void initRotEncoder() {
+    pinMode(ROT_ENCODER_OUTPUT_A, INPUT);
+    pinMode(ROT_ENCODER_OUTPUT_B, INPUT);
 
-  Serial.print("Wifi: Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-
-inline void initButton() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.println("Button: Setup done");
-}
-
-inline void initLEDs() {
-  pinMode(LED_RED_PIN, OUTPUT);
-  pinMode(LED_GREEN_PIN, OUTPUT);
-  pinMode(LED_BLUE_PIN, OUTPUT);
-
-  digitalWrite(LED_GREEN_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                        // wait for a second
-  digitalWrite(LED_GREEN_PIN, LOW);   // turn the LED off by making the voltage LOW
-  Serial.println("LED: Setup done");
-}
-
-void setup() {
-  Serial.begin(115200);
-  while (!Serial) {
-      Serial.println("WAITING");
+    prevRotationA = digitalRead(ROT_ENCODER_OUTPUT_A);
   }
-Serial.println("DONE");
-  initLEDs();
-  initButton();
-  initWifi();
-  // initMqtt();
 
-  Serial.println("Setup done");
-}
+  inline void initWifi() {
+    WiFi.begin(ssid, password);
+    WiFi.config(ip, gateway, subnet);
+    Serial.print("Wifi: Connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(300);
+      Serial.print(".");
+    }
+
+    Serial.println();
+
+    Serial.print("Wifi: Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
+    Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+  }
+
+  inline void initButton() {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    Serial.println("Button: Setup done");
+  }
+
+  inline void initLEDs() {
+    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(LED_GREEN_PIN, OUTPUT);
+    pinMode(LED_BLUE_PIN, OUTPUT);
+
+    digitalWrite(LED_GREEN_PIN, HIGH);
+    Serial.println("LED: Setup done");
+  }
+
+  void setup() {
+    Serial.begin(115200);
+    while (!Serial) {
+    }
+
+    initLEDs();
+    initButton();
+    initWifi();
+    initVibration();
+
+    digitalWrite(LED_GREEN_PIN, LOW);
+
+    Serial.println("Setup done");
+  }
