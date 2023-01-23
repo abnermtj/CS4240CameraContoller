713b3fe746cbd07723674743a2ff9263e709f116
abner <abnermtj@live.com.sg>
2023-01-23 23:03:01 +0800

    Add unity sample level

---
+++ CameraVrController/Assets/Scripts/InputController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System;
+
+public class InputController
+{
+    public int CurrentValue = 2;
+
+    public void Begin(string ipAddress, int port)
+    {
+        var thread = new Thread(() =>
+        {
+            try
+            {
+                while (true)
+                {
+                    TcpClient socketConnection = new TcpClient(ipAddress, port);
+                    Byte[] bytes = new Byte[1024];
+                    // Get a stream object for reading
+                    using (NetworkStream stream = socketConnection.GetStream())
+                    {
+                        int length;
+                        // Read incomming stream into byte arrary.
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+                            // Convert byte array to string message.
+                            string serverMessage = Encoding.ASCII.GetString(incommingData);
+
+                            if (serverMessage != "\r")
+                            {
+                                CurrentValue = int.Parse(serverMessage);
+                                Debug.Log("server message received as: " + serverMessage);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SocketException socketException)
+            {
+                Debug.Log("Socket exception: " + socketException);
+            }
+        });
+
+        thread.Start();
+    }
+}
---
+++ CameraVrController/Assets/Scripts/Manager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private InputController inputController;
+    private bool waitState = true;
+    public GameObject cube;
+
+    private const string ip = "192.168.86.26";
+    private const int port = 80;
+
+    private void Start()
+    {
+        //This will do the network stuff
+        inputController = new InputController();
+        inputController.Begin(ip, port);
+    }
+
+    private void Update()
+    {
+        /*        if (waitState)
+                {
+                    waitState = false;
+        */
+        Signal();
+    }
+
+    private int prevButtonValue = 3;
+
+    public void Signal()
+    {
+        //Debug.Log(inputController.CurrentValue & 1);
+
+        if ((inputController.CurrentValue & 1) != prevButtonValue)
+        {
+            if ((inputController.CurrentValue & 1) == 1)
+            {
+                //Debug.Log("Button Not Pressed");
+                cube.GetComponent<Renderer>().materials[0].color = Color.black;
+            }
+            else
+            {
+                //Debug.Log("Button Pressed");
+                cube.GetComponent<Renderer>().materials[0].color = Color.blue;
+            }
+            prevButtonValue = inputController.CurrentValue & 1;
+        }
+
+        if ((inputController.CurrentValue & 6) == 4)
+        {
+            Debug.Log("Encoder Clockwise Rot");
+            cube.GetComponent<Renderer>().materials[0].color += new Color(0, 0.04f, .04f, 0);
+        }
+        else if ((inputController.CurrentValue & 6) == 2)
+        {
+            Debug.Log("Encoder Anti Clockswise Rot");
+            cube.GetComponent<Renderer>().materials[0].color -= new Color(0, 0.04f, 0.04f, 0);
+        }
+
+        //StartCoroutine(Wait());
+    }
+
+    /*    private IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(2);
+            waitState = true;
+        }
+    */
+}
