f7e93c6c1c6e373da825fb5de1765ab29734d6e6
abner <abnermtj@live.com.sg>
2023-02-06 16:02:07 +0800

    Add wifi manager

--- sketch_jan22b/sketch_jan22b.ino
+++ sketch_jan22b/sketch_jan22b.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
 #include <WiFiClient.h>
+#include <WiFiManager.h>
+#include <FS.h>
 
 // ---------------------------- PINOUT -------------------------------
 #define BUTTON_PIN D1
@@ -21,7 +23,7 @@ int curButtonState = 0;
 
 // ---------------------------- Wifi -------------------------------
 #define SEND_DELAY_MS 1
-const char* ssid = "tolentino";     //put your wifi network name here
+const char* ssid = "Abner";         //put your wifi network name here
 const char* password = "Abner123";  //put your wifi password here
 
 WiFiServer server(80);
@@ -31,6 +33,17 @@ WiFiClient client;
 IPAddress ip(192, 168, 86, 26);
 IPAddress gateway(192, 168, 86, 1);
 IPAddress subnet(255, 255, 255, 0);
+
+// WiFi Manager
+WiFiClient wifiClient;
+WiFiManager wifiManager;
+bool configPortal = false;
+char username[20] = "user1";
+WiFiManagerParameter custom_username("username", "username", username, 20);
+char broadcast_ssid[50] = "CS4240 yay";
+
+const char* WIFI_AP_SSID = "CS4240 yay";
+const char* WIFI_AP_PASSWORD = "CS4240-123";
 
 int prevSendTime = 0;
 
@@ -59,6 +72,46 @@ void loopVibration() {
       break;
   }
 }
+
+// store username in file after configuration is updated
+void saveConfigCallback() {
+  Serial.println("WiFi config: username updated");
+
+  // copy username into variable
+  strcpy(username, custom_username.getValue());
+
+  // save username to filesystem
+  File usernameFile = SPIFFS.open("/username.txt", "w");
+  int bytesWritten = usernameFile.print(username);
+  if (bytesWritten > 0) {
+    Serial.println("username saved successfully");
+  } else {
+    Serial.println("username save failed");
+  }
+  usernameFile.close();
+
+  // if refreshed through config portal
+  if (configPortal) {
+    ESP.restart();
+  }
+}
+
+
+void readUsername() {
+  if (SPIFFS.exists("/username.txt")) {
+    File usernameFile = SPIFFS.open("/username.txt", "r");
+    if (usernameFile) {
+      size_t filesize = usernameFile.size();
+      usernameFile.readBytes(username, filesize);
+      usernameFile.close();
+    } else {
+      Serial.println("error opening file for reading");
+    }
+  } else {
+    Serial.println("username file does not exist, will be created after connecting to WiFi");
+  }
+}
+
 void loopButton() {
   curButtonState = digitalRead(BUTTON_PIN);
   sendPacket.isButtonPressed = curButtonState;
@@ -95,79 +148,103 @@ void loopRotEncoder() {
   }
 }
 
-  void loop() {
-    loopButton();
-    loopRotEncoder();
-    loopVibration();
+void loop() {
+  loopButton();
+  loopRotEncoder();
+  loopVibration();
+  wifiManager.process();
 
-    client = server.accept();
-    int curTime = millis();
-    if (client && (curTime - prevSendTime >= SEND_DELAY_MS)) {
-      prevSendTime = curTime;
+  client = server.accept();
+  int curTime = millis();
+  if (client && (curTime - prevSendTime >= SEND_DELAY_MS)) {
+    prevSendTime = curTime;
 
-      // Packet structure
-      // Bit 2-3 Rot encoder status
-      // Bit 1 Button status
-      int packet = (sendPacket.rotEnconderStatus << 1) | (sendPacket.isButtonPressed);
-      client.print(packet);
+    // Packet structure
+    // Bit 2-3 Rot encoder status
+    // Bit 1 Button status
+    int packet = (sendPacket.rotEnconderStatus << 1) | (sendPacket.isButtonPressed);
+    client.print(packet);
 
-      sendPacket.rotEnconderStatus = 0;
-    }
+    sendPacket.rotEnconderStatus = 0;
   }
+}
 
-  inline void initVibration() {
-    pinMode(VIBRATION_PIN, OUTPUT);
+inline void initVibration() {
+  pinMode(VIBRATION_PIN, OUTPUT);
+}
+inline void initRotEncoder() {
+  pinMode(ROT_ENCODER_OUTPUT_A, INPUT);
+  pinMode(ROT_ENCODER_OUTPUT_B, INPUT);
+
+  prevRotationA = digitalRead(ROT_ENCODER_OUTPUT_A);
+}
+
+inline void initWifi() {
+  // set up filesystem and read username
+  SPIFFS.begin();
+  readUsername();
+
+  // WiFi.begin(ssid, password);
+  WiFi.config(ip, gateway, subnet);
+  // Serial.print("Wifi: Connecting");
+
+  // connect to WiFi using WiFiManager
+  // stores credentials internally in the wemos, and if fails to connect, creates AP with captive portal
+  wifiManager.setSaveConfigCallback(saveConfigCallback);  // only called when user updates WiFi credentials (if wifi credentials are saved, not called)
+  wifiManager.addParameter(&custom_username);
+  if (!wifiManager.autoConnect(WIFI_AP_SSID, WIFI_AP_PASSWORD)) {
+    ESP.restart();
   }
-  inline void initRotEncoder() {
-    pinMode(ROT_ENCODER_OUTPUT_A, INPUT);
-    pinMode(ROT_ENCODER_OUTPUT_B, INPUT);
+  delay(1000);
 
-    prevRotationA = digitalRead(ROT_ENCODER_OUTPUT_A);
-  }
+  strncat(broadcast_ssid, username, 32);
 
-  inline void initWifi() {
-    WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_AP_STA);
+  configPortal = true;
+  wifiManager.setBreakAfterConfig(saveConfigCallback);
+  wifiManager.setConfigPortalBlocking(false);                       // use this as softAP which we were using previously
+  wifiManager.startConfigPortal(broadcast_ssid, WIFI_AP_PASSWORD);  // TODO: ESP crashes if changing SSID on this portal (will fix)
+  Serial.printf("Broadcasting for wemos-presence on SSID: %s\n", broadcast_ssid);
+  // Serial.print("Wifi: Connected, IP address: ");
     WiFi.config(ip, gateway, subnet);
-    Serial.print("Wifi: Connecting");
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(300);
-      Serial.print(".");
-    }
-
-    Serial.println();
-
-    Serial.print("Wifi: Connected, IP address: ");
-    Serial.println(WiFi.localIP());
-
-    server.begin();
-    Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+    Serial.print(".");
   }
 
-  inline void initButton() {
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-    Serial.println("Button: Setup done");
+
+  Serial.print("Wifi: Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+}
+
+inline void initButton() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.println("Button: Setup done");
+}
+
+inline void initLEDs() {
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
+
+  digitalWrite(LED_GREEN_PIN, HIGH);
+  Serial.println("LED: Setup done");
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
   }
 
-  inline void initLEDs() {
-    pinMode(LED_RED_PIN, OUTPUT);
-    pinMode(LED_GREEN_PIN, OUTPUT);
-    pinMode(LED_BLUE_PIN, OUTPUT);
+  initLEDs();
+  initButton();
+  initWifi();
+  initVibration();
 
-    digitalWrite(LED_GREEN_PIN, HIGH);
-    Serial.println("LED: Setup done");
-  }
+  digitalWrite(LED_GREEN_PIN, LOW);
 
-  void setup() {
-    Serial.begin(115200);
-    while (!Serial) {
-    }
-
-    initLEDs();
-    initButton();
-    initWifi();
-    initVibration();
-
-    digitalWrite(LED_GREEN_PIN, LOW);
-
-    Serial.println("Setup done");
-  }
+  Serial.println("Setup done");
+}
